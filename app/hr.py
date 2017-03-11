b3a0bdafdf3492e35497018a7695f7aba74a162c
kvajapey1108 <kvajapey@gmail.com>
2017-03-11 18:42:20 -0500

    hr in progress for construction

--- app/hr.py
+++ app/hr.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import (
+    Flask,
+    abort,
+    flash,
+    redirect,
+    render_template,
+    request,
+    url_for,
+    current_app,
+    session,
+)
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "<h1 style='color:blue'>Hello There!</h1>"
+	return render_template("construction.html")
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port=8400, debug=True)
